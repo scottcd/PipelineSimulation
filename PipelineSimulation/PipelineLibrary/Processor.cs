bfd198ab55cd48a01d009be6aeb11c906f2ab48f
Chandler <74741436+scottcd@users.noreply.github.com>
2021-11-03 08:39:18 -0400

    Add files via upload

---
+++ PipelineSimulation/PipelineLibrary/Processor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PipelineLibrary {
+    public class Processor {
+        public List<IInstruction> InstructionMemory;
+        public IInstruction [] Pipeline { get; set; }
+        public LogicController ControlLogic { get; set; }
+        // ALUs
+        // Registers
+        // MUXs
+
+        public Processor() {
+            Pipeline = new IInstruction[5];
+            InstructionMemory = new List<IInstruction>();
+            ControlLogic = new LogicController();
+        }
+
+        // fetch
+
+        // decode
+
+        // execute
+
+        // memaccess
+
+        // regwrite
+    }
+}
